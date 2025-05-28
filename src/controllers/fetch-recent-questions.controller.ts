2f73410e5ad1f98c244e5ef35d3fd328375a0497
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2025-05-28 15:39:28 -0300

    Adds recent questions endpoint with pagination
    Introduces a new authenticated endpoint to fetch recent questions with basic page-based pagination. Improves API usability and supports better client-side data fetching by allowing retrieval of questions in a paged manner. Also fixes the validation pipe to correctly return parsed values and updates the sample HTTP client for the new endpoint.

---
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Query, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const pageQueryParamsSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamsSchema)
+
+type PageQueryParamsSchema = z.infer<typeof pageQueryParamsSchema>
+
+@Controller('questions')
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionsController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get()
+  async handle(
+    @Query('page', queryValidationPipe) page: PageQueryParamsSchema,
+  ) {
+    const perPage = 1
+
+    const questions = await this.prisma.question.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: perPage, // Limit the number of questions returned
+      skip: (page - 1) * perPage, // Skip questions based on the page number
+    })
+    return { questions }
+  }
+}
