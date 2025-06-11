ff1934811c9523a54c8b5f8a7f47647934ba8ded
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2025-06-11 18:04:23 -0300

    Add student authentication and registration use cases
    Implements use cases for student authentication and registration,
    including error handling for invalid credentials and duplicate
    registrations. Introduces utility classes for cryptography and
    repositories to facilitate these operations.

---
+++ src/domain/forum/application/use-cases/authenticate-student.ts
@@ -0,0 +1,54 @@
+import { Either, left, right } from '@/core/either'
+import { Injectable } from '@nestjs/common'
+import { StudentsRepository } from '../repositories/students-repository'
+import { HashComparer } from '../cryptography/hash-comparer'
+import { Encrypter } from '../cryptography/encrypter'
+import { WrongCredentialsError } from './errors/wrong-credentials-error'
+
+interface AuthenticateStudentUseCaseRequest {
+  email: string
+  password: string
+}
+
+type AuthenticateStudentUseCaseResponse = Either<
+  WrongCredentialsError,
+  {
+    accessToken: string
+  }
+>
+@Injectable()
+export class AuthenticateStudentUseCase {
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private hashComparer: HashComparer,
+    private encryptor: Encrypter,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateStudentUseCaseRequest): Promise<AuthenticateStudentUseCaseResponse> {
+    const student = await this.studentsRepository.findByEmail(email)
+
+    if (!student) {
+      return left(new WrongCredentialsError())
+    }
+
+    const passwordMatches = await this.hashComparer.compare(
+      password,
+      student.password,
+    )
+
+    if (!passwordMatches) {
+      return left(new WrongCredentialsError())
+    }
+
+    const accessToken = await this.encryptor.encrypt({
+      sub: student.id.toString(),
+    })
+
+    return right({
+      accessToken,
+    })
+  }
+}
