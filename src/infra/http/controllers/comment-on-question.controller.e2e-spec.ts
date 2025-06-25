8e12c5cf6e9a6bb90419bb30d4375a359f1948f5
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2025-06-25 17:44:05 -0300

    Adds support for commenting on questions
    Integrates the CommentOnQuestion functionality into the HTTP module by adding relevant controllers and use cases. Marks the CommentOnQuestion use case as injectable for dependency management. Updates the endpoint and request path in the e2e test for choosing the best answer to ensure correctness and consistency.

--- src/infra/http/controllers/comment-on-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/comment-on-question.controller.e2e-spec.ts
@@ -5,35 +5,32 @@ import { INestApplication } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
 import request from 'supertest'
-import { AnswerFactory } from 'test/factories/make-answer'
 import { QuestionFactory } from 'test/factories/make-question'
 import { StudentFactory } from 'test/factories/make-student'
 
-describe('Choose question best answer (e2e)', () => {
+describe('Comment on question (e2e)', () => {
   let app: INestApplication
-  let jwt: JwtService
   let prisma: PrismaService
   let studentFactory: StudentFactory
   let questionFactory: QuestionFactory
-  let answerFactory: AnswerFactory
+  let jwt: JwtService
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory, QuestionFactory, AnswerFactory],
+      providers: [StudentFactory, QuestionFactory],
     }).compile()
 
     app = moduleRef.createNestApplication()
     prisma = moduleRef.get(PrismaService)
     studentFactory = moduleRef.get(StudentFactory)
     questionFactory = moduleRef.get(QuestionFactory)
-    answerFactory = moduleRef.get(AnswerFactory)
     jwt = moduleRef.get(JwtService)
 
     await app.init()
   })
 
-  test('[PATCH] /answer/:answerId/answers', async () => {
+  test('[POST] /questions/:questionsId/comments', async () => {
     const user = await studentFactory.makePrismaStudent()
 
     const accessToken = jwt.sign({ sub: user.id.toString() })
@@ -42,24 +39,21 @@ describe('Choose question best answer (e2e)', () => {
       authorId: user.id,
     })
 
-    const answer = await answerFactory.makePrismaAnswer({
-      authorId: user.id,
-      questionId: question.id,
-      content: 'First Test Answer',
-    })
-
-    const answerId = answer.id.toString()
+    const questionsId = question.id.toString()
 
     const response = await request(app.getHttpServer())
-      .patch(`/answer/${answerId}/choose-as-best`)
+      .post(`/questions/${questionsId}/comments`)
       .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        content: 'New cooment on question',
+      })
 
-    expect(response.status).toBe(204)
+    expect(response.status).toBe(201)
 
-    const questionOnDatabase = await prisma.question.findUnique({
-      where: { id: question.id.toString() },
+    const commentOnDataBase = await prisma.comment.findFirst({
+      where: { content: 'New cooment on question' },
     })
 
-    expect(questionOnDatabase?.bestAnswerId).toEqual(answerId)
+    expect(commentOnDataBase).toBeTruthy()
   })
 })
