73463bb955bda646b4c9519438fe3ff610f13b71
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2025-09-08 12:36:38 -0300

    Adds attachment upload API with validation
    Introduces a new `POST /attachments` endpoint to handle file uploads. This endpoint leverages NestJS's `FileInterceptor` and `ParseFilePipe` to validate uploaded files, enforcing a maximum size of 2MB and allowing only `png`, `jpg`, `jpeg`, and `pdf` file types.
    
    Includes comprehensive end-to-end tests to verify the endpoint's functionality and integration. Also adds necessary Multer type definitions and module configuration to support the file upload mechanism.

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -40,6 +40,7 @@ import { FetchQuestionCommentsController } from './controllers/fetch-question-co
 import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-question-comments'
 import { FetchAnswerCommentsController } from './controllers/fetch-answer-comments.controller'
 import { FetchAnswerCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-answer-comments'
+import { UploadAttachmentController } from './controllers/upload-attachment.controller'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -62,6 +63,7 @@ import { FetchAnswerCommentsUseCase } from '@/domain/forum/application/use-cases
     DeleteAnswerCommentController,
     FetchQuestionCommentsController,
     FetchAnswerCommentsController,
+    UploadAttachmentController,
   ],
   providers: [
     CreateQuestionUseCase,
